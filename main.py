bd1b1fe9c5e68bb0399e6ca465e346afc717fd68
Shashank Srivastava <shashank@Shashanks-MacBook-Air.local>
2025-02-15 21:38:17 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.core.agent import ReActAgent
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.experimental.query_engine import PandasQueryEngine
+from llama_index.llms.openai import OpenAI
+
+from prompts import new_prompt, instruction_str, context
+from note import note_engine
+
+load_dotenv()
+population_path = os.path.join('data', 'world_population.csv')
+population_df = pd.read_csv(population_path)
+#print(population_df.head())
+population_query_engine = PandasQueryEngine(df=population_df, verbose=True, instruction_str=instruction_str)
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+#population_query_engine.query("what is the population of canada")
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine,
+        metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics",
+        ),
+    )
+]
+
+llm = OpenAI(model="gpt-3.5-turbo-16k")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
+
